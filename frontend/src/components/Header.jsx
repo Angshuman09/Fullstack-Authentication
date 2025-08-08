6ee686a0298959996115f80911194ae9f4273b2c
Angshuman09 <angshumankashyap69yap69@gmail.com>
2025-08-08 17:19:57 +0530

    make responsive home page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='bg-[#F5DF4D] flex justify-center items-center w-full min-h-screen overflow-hidden flex-col text-center px-4 gap-3.5'>
+      <h1 className=' font-bold text-[15vw] tracking-tight leading-tight bg-clip-text text-transparent bg-linear-65 from-purple-500 to-pink-500'>Hello devs</h1>
+      <p className='font-light flex justify-center items-center'>Connecting developers. Creating solutions.</p>
+      <button className='bg-red-300 p-4 cursor-pointer rounded-2xl hover:bg-pink-400 font-mono'>Get Started</button>
+    </div>
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import {useNavigate} from 'react-router-dom';
 function Navbar() {
+  const navigate = useNavigate();
   return (
-    
-
-<nav class=" border-gray-200 bg-yellow-100">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <span class="self-center text-2xl font-semibold whitespace-nowrap text-zinc-500">OAuth</span>
-  </a>
-  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-
-      <button type="button" class="text-white bg-yellow-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center hover:bg-amber-400">Login</button>
-  </div>
-  </div>
-</nav>
-
-  )
+    <nav class=" border-gray-200 bg-[#F5DF4D] fixed w-full">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="https://flowbite.com/"
+          class="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span class="self-center text-2xl font-semibold whitespace-nowrap text-zinc-900">
+            OAuth
+          </span>
+        </a>
+        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+          onClick={()=>navigate('/login')}
+            type="button"
+            class="text-zinc-600 font-mono font-bold hover:bg-pink-400 bg-pink-300 rounded-lg text-sm px-5 py-3 text-center"
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
