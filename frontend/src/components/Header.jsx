bf599e1382aa03b436d4e06a589a71a78a0faa8b
Angshuman09 <angshumankashyap69yap69@gmail.com>
2025-08-10 02:44:29 +0530

    fix the ui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='bg-[#F5DF4D] flex justify-center items-center w-full min-h-screen overflow-hidden flex-col text-center px-4 gap-3.5'>
-      <h1 className=' font-bold text-[15vw] tracking-tight leading-tight bg-clip-text text-transparent bg-linear-65 from-purple-500 to-pink-500'>Hello devs</h1>
+    <div className="bg-[#fefcff] relative flex justify-center items-center w-full min-h-screen overflow-hidden flex-col text-center px-4 gap-3.5">
+  {/* Dreamy Sky Pink Glow */}
+  <div
+    className="absolute inset-0 z-0"
+    style={{
+      backgroundImage: `
+        radial-gradient(circle at 30% 70%, rgba(173, 216, 230, 0.35), transparent 60%),
+        radial-gradient(circle at 70% 30%, rgba(255, 182, 193, 0.4), transparent 60%)`,
+    }}
+  />
+      <h1  className=' font-bold text-[15vw] tracking-tight leading-tight bg-clip-text text-transparent bg-linear-65 from-purple-500 to-pink-500 '>
+        <span style={{ fontFamily: 'Pacifico, cursive' }}  >
+        Hello{' '}
+        </span>
+         <span className="relative inline-flex sm:inline">
+                        <span className="bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-lg filter opacity-30 w-full h-full absolute inset-0 z-10 rounded-4xl"></span>
+                        <span className="relative z-30 text-white"> dev.</span>
+                    </span>
+      </h1>
       <p className='font-light flex justify-center items-center'>Connecting developers. Creating solutions.</p>
-      <button className='bg-red-300 p-4 cursor-pointer rounded-2xl hover:bg-pink-400 font-mono'>Get Started</button>
+      <button className='bg-[#121212] p-4 cursor-pointer rounded-2xl hover:bg-black transition-colors duration-75 font-mono text-amber-50 relative z-40'>Get Started</button>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import {useNavigate} from 'react-router-dom';
+import mysvg from '../assets/svg.svg';
 function Navbar() {
   const navigate = useNavigate();
   return (
-    <nav class=" border-gray-200 bg-[#F5DF4D] fixed w-full">
+    <nav class=" border-gray-200 fixed w-full z-50">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <span class="self-center text-2xl font-semibold whitespace-nowrap text-zinc-900">
+        <div>
+          {/* <img src={mysvg} className="w-2.5 h-auto absolute z-50" alt="" /> */}
+          <span style={{ fontFamily: 'Pacifico, cursive' }} class="self-center text-2xl whitespace-nowrap text-emerald-200 font-thin">
             OAuth
           </span>
-        </a>
+        </div>
         <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
           onClick={()=>navigate('/login')}
             type="button"
-            class="text-zinc-600 font-mono font-bold hover:bg-pink-400 bg-pink-300 rounded-lg text-sm px-5 py-3 text-center"
+            class="text-zinc-600 font-mono font-bold hover:bg-pink-400 bg-pink-300 rounded-lg text-sm px-5 py-3 text-center cursor-pointer"
           >
             Login
           </button>
